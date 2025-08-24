25e02f3e1173158eed33223a2f585a090bc3bb50
rolandjutai <roland.jutai@simplehomecinema.com>
2025-08-24 21:54:30 +1000

    Update controlsd.py - making sure we don't disable lat on gas pedal

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -299,8 +299,9 @@ class Controls:
       self.events.add(EventName.resumeBlocked)
 
     # Disable on rising edge of accelerator or brake. Also disable on brake when speed > 0
-    if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or \
-      (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or \
+    if self.CP.openpilotLongitudinalControl:
+      if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or
+      (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or
       (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
       self.events.add(EventName.pedalPressed)
 
