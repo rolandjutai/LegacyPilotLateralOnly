b09855fea534b7a06aca00a9d2071155e506f883
rolandjutai <roland.jutai@simplehomecinema.com>
2025-08-24 21:58:04 +1000

    Update controlsd.py - syntax error

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -299,11 +299,12 @@ class Controls:
       self.events.add(EventName.resumeBlocked)
 
     # Disable on rising edge of accelerator or brake. Also disable on brake when speed > 0
+    # Only applicable when openpilot is actually doing longitudinal control.
     if self.CP.openpilotLongitudinalControl:
-      if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or
-      (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or
-      (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
-      self.events.add(EventName.pedalPressed)
+      if ((CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or
+          (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or
+          (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill))):
+        self.events.add(EventName.pedalPressed)
 
     if CS.brakePressed and CS.standstill:
       self.events.add(EventName.preEnableStandstill)
